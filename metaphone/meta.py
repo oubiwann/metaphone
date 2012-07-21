4d92f96a7bcd9e04da57631e8f31acb5e3992bac
Duncan McGreggor <duncan@dreamhost.com>
2012-07-21 01:18:56 -0400

    Added metaphone package.

---
+++ metaphone/meta.py
@@ -0,0 +1,8 @@
+display_name = "Double Metaphone"
+library_name = "metaphone"
+version = "0.1"
+author = "Andrew Collins"
+author_email = "AtomBoy@SWCP.com"
+license = "BSD"
+url = "https://github.com/oubiwann/double-metaphone"
+description = "A Python implementation of the double-metaphone algorithm."
