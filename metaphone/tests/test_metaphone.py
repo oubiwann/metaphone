f987c2577895c09d02a801c80c7d65d504d98b2c
Duncan McGreggor <duncan@dreamhost.com>
2012-07-21 19:00:41 -0400

    Added unit tests for the new Word object.

--- metaphone/tests/test_metaphone.py
+++ metaphone/tests/test_metaphone.py
@@ -1,7 +1,45 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from metaphone.metaphone import doublemetaphone
+from metaphone.metaphone import doublemetaphone, Word
+
+
+class WordTestCase(unittest.TestCase):
+    """
+    """
+    def test_init(self):
+        word = Word("stupendous")
+        self.assertEqual(word.original, "stupendous")
+        self.assertEqual(word.decoded, "stupendous")
+        self.assertEqual(word.normalized, "stupendous")
+        self.assertEqual(word.upper, "STUPENDOUS")
+        self.assertEqual(word.length, 10)
+        self.assertEqual(word.buffer, u"--STUPENDOUS------")
+
+    def test_init_unicode(self):
+        word = Word("naïve")
+        self.assertEqual(word.original, "na\xc3\xafve")
+        self.assertEqual(word.decoded, u"na\xefve")
+        self.assertEqual(word.normalized, "naive")
+        self.assertEqual(word.upper, "NAIVE")
+        self.assertEqual(word.length, 5)
+        self.assertEqual(word.buffer, u"--NAIVE------")
+
+    def test_is_slavo_germanic(self):
+        word = Word("Berkowitz")
+        self.assertTrue(word.is_slavo_germanic)
+        word = Word("Czeck")
+        self.assertTrue(word.is_slavo_germanic)
+        word = Word("Bob")
+        self.assertFalse(word.is_slavo_germanic)
+
+    def test_first_letter(self):
+        word = Word("naïve")
+        self.assertEqual(word.first_letter, "N")
+
+    def test_first_2_letters(self):
+        word = Word("naïve")
+        self.assertEqual(word.first_2_letters, "NA")
 
 
 class MetaphoneTestCase(unittest.TestCase):
